796614205f92f95e0d4d128f24332ddc3b4349f9
Tarkan1857 <t.kemalzade@hotmail.co.uk>
2016-08-09 20:12:16 +0100

    Created a button class to be able to creat buttons to manipulate through different screens through out the game

--- Pinball Game/Pinball Game/include/Utilities/button.h
+++ Pinball Game/Pinball Game/include/Utilities/button.h
@@ -5,12 +5,22 @@
 
 #include <Graphics\rectangle.h>
 #include <SFML\Graphics.hpp>
+
 class Button : public Rectangle, sf::Drawable
 {
+	enum ButtonState {DEFAULT, HOVERED, CLICKED};
+
 	public:
 		Button();
-		Button(sf::Image*, sf::Image*, int, int, math::Vector2D);
+		Button(int, int, math::Vector2D);
+		~Button();
+
+		void updateImage(ButtonState);
+
+		void draw(sf::RenderTarget &target, sf::RenderStates states) const;
+
 	private:
+		sf::RectangleShape m_ButtonShape;
 };
 #endif // !_BUTTON_H
 
